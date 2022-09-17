0049f007cfd164aa43ac4d0c3356a9ea02df781a
BunbunTheVile <evolvingbunny@gmail.com>
2022-09-17 22:05:39 +0200

    wip: add Note class

---
+++ Backend/MirasMentalNotes/MirasMentalNotes/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace MirasMentalNotes
+{
+    public class Note
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Content { get; set; }
+        public List<string> Tags { get; set; } = new();
+    }
+}
