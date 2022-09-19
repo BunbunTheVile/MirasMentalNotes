da067aab85e290d081d10796338bf8e2f77c44de
BunbunTheVile <evolvingbunny@gmail.com>
2022-09-19 15:59:23 +0200

    wip: add empty recent-notes component

---
+++ Frontend/miras-mental-notes/src/app/recent-notes/recent-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-notes',
+  templateUrl: './recent-notes.component.html',
+  styleUrls: ['./recent-notes.component.css']
+})
+export class RecentNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
